67e1fac389cafb3786eb82bffcfd80844ea1972f
Batishavo <romero.martinez.carlosraul@gmail.com>
2021-08-02 11:49:00 -0500

    Video 44

---
+++ 43-49. MVC/MyVersion/controllers/main.php
@@ -0,0 +1,11 @@
+<?php
+    class Main{
+        function __construct(){
+            echo "<p>Nuevo controlador Main</p>";
+        }
+        function saludo(){
+            echo "<p>Ejecuta el método saludo</p>";
+        }
+    }
+   
+?>
---
+++ 43-49. MVC/MyVersion/index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'libs/app.php';
+    //echo "<p></p>hola bb</p>";
+    $app=new App();
+    //echo "xdexd";
+?>
